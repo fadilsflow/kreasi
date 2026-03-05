e3c11a23574df4e09bd4564a2bc66dbbc8343453
Wahyu Akhmad Fadillah <Wahyufadil1140@gmail.com>
2026-03-06 01:05:41 +0700

    introduce public profile footer comp

---
+++ src/components/public-profile-footer.tsx
@@ -0,0 +1,11 @@
+import Credits from "./Credits";
+
+export default function PublicProfileFooter() {
+    return <footer>
+        <div className="flex justify-between">
+            <div className="flex gap-2 items-center">
+                <Credits />
+            </div>
+        </div>
+    </footer>
+}
