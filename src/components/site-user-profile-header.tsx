ec6d65a2ef34ff8d740dc5756e4b86e71d43b72d
Wahyu Akhmad Fadillah <Wahyufadil1140@gmail.com>
2026-03-14 04:56:32 +0700

    love this

--- src/components/site-user-profile-header.tsx
+++ src/components/site-user-profile-header.tsx
@@ -1,30 +1,28 @@
-import { Menu } from 'lucide-react'
+import { Ellipsis } from 'lucide-react'
 import { useState } from 'react'
 import UserButton from './user-button'
 import { Button } from './ui/button'
 import { SavedDrawer } from './saved-drawer'
-import { LogoMark } from './kreasi-logo'
+// import { LogoMark } from './kreasi-logo'
 
 
-export default function SiteUserProfileHeader() {
+export default function SiteUserProfileHeader({ textColor, className }: { textColor: string, className: string }) {
   const [isSavedOpen, setIsSavedOpen] = useState(false)
 
   return (
     <>
-      <header className="z-50 px-2">
+      <header className={`z-50 px-2 ${className}`}>
         <div className="mx-auto sm:max-w-2xl md:max-w-3xl">
-          <div className="flex h-16 items-center justify-between px-3">
-            <LogoMark />
-
-
+          <div className="flex h-16 items-center justify-end px-3">
+            {/* <LogoMark  style={{ color: textColor }} /> */}
             <div className="flex items-center gap-3">
               <UserButton />
               <Button
-                variant="outline"
+                variant="ghost"
                 className="relative"
                 onClick={() => setIsSavedOpen(true)}
               >
-                <Menu className="h-4 w-4 fill-current" />
+                <Ellipsis style={{ color: textColor }} className="h-4 w-4 fill-current" />
               </Button>
             </div>
           </div>
